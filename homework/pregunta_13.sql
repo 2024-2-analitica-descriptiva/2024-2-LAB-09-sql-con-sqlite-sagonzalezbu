857a57bbf06eeb27e377fc1fa22c56ec87c69870
sagonzalezbu <70988399+sagonzalezbu@users.noreply.github.com>
2024-12-13 22:47:44 -0500

    Arreglando 13

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -43,4 +43,9 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT k0, avg(c12) from tbl1 where c13>400 GROUP BY k0;
+--SELECT k0, avg(c12) from tbl1 where c13>400 GROUP BY k0;
+
+SELECT k0, ROUND(AVG(c12), 2) AS 'avg(c12)'
+FROM tbl1 
+WHERE c13 > 400 
+GROUP BY k0;
